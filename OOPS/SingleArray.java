6c20dd86de59b5d365f6ad4252d74b6cff1b5e47
Nikhila Baby <128206204+nikhi-la@users.noreply.github.com>
2024-03-14 11:29:12 +0530

    Add files via upload

---
+++ OOPS/SingleArray.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class Array1
+{
+	int[] res;
+
+	int[] compute(int[] s)
+	{
+		res=new int[s.length];
+
+		for(int i=0;i<s.length;++i)
+		{
+			res[i]=s[i]+10;
+		}
+		
+		return res;
+	}	
+}
+
+
+class SingleArray
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		Array1 a1=new Array1();		
+
+		System.out.println("Enter Size of Array");
+		int n=s.nextInt();
+
+		int[] a=new int[n];
+		int[] b=new int[n];
+
+		System.out.println("Enter "+n+" Elements");
+		for(int i=0;i<n;++i)
+		{
+			a[i]=s.nextInt();
+		}
+		
+		System.out.println("\n\nArray Elements");
+		for(int i=0;i<n;++i)
+		{
+			System.out.print(a[i]+" ");
+		}
+
+		b=a1.compute(a);
+		
+		System.out.println("\n\nArray After Increment");
+		for(int i=0;i<n;++i)
+		{
+			System.out.print(b[i]+" ");
+		}
+	}
+
+}
