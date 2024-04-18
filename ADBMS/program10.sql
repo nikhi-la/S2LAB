bf60a347452fa37edcfa25f13bd3601fb7d3dac2
Nikhila Baby <128206204+nikhi-la@users.noreply.github.com>
2024-04-18 21:10:08 +0530

    Update program10.sql

--- ADBMS/program10.sql
+++ ADBMS/program10.sql
@@ -1,23 +1,23 @@
+
 set serveroutput on
 declare
 	r number(6,2);
 	ch number(6):=0;
-	area number(6,4);
+	area number(6,4):=0;
 begin
-	loop
 		r:=&r;
+
 		if(r<1)
 		then
 			dbms_output.put_line('Radius less than 1');
 
 		else
-			area:=3.14*r*r;
+			for i in 1..r
+			loop
+				area:=area+r*3.14;
+				end loop;
 			dbms_output.put_line('Area : '|| area);
-			ch:=ch+1;
-			if(ch=2)then
-			break;
-			end if;
 		end if;	
-	end loop;	
+
 end;
 /
