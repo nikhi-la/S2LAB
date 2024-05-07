85a4c3a361096124a9454276f49f00fc5823ebc8
Nikhila Baby <128206204+nikhi-la@users.noreply.github.com>
2024-05-07 14:57:27 +0530

    Add files via upload

---
+++ OOPS/File/Question17.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+class Question17
+{
+    public static void main(String args[]) throws IOException
+    {
+        InputStreamReader r=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(r);
+        
+        FileWriter f=new FileWriter("D:/Nikhila/OOPS/File/input.txt");
+        BufferedWriter bw=new BufferedWriter(f);
+        System.out.println("Enter Numnber of lines");
+        int n=
+        System.out.println("Enter Content(Enter end to exit)");
+        String s;
+        int i=0;
+        do{
+            if(i==n)
+                break;
+            s=br.readLine();
+            bw.write(s);
+            bw.write("\n");
+            i++;
+        }while(true);
+        bw.close();
+
+        FileReader fr=new FileReader("D:/Nikhila/OOPS/File/input.txt");
+        BufferedReader bfr=new BufferedReader(fr);
+
+        String sr;
+        System.out.println("\n----The file is----\n");
+        while((sr=bfr.readLine())!=null)
+        {
+            System.out.println(sr);
+        }
+        bfr.close();
+
+    }
+}
